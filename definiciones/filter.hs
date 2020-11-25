12600ed1c23eca5d561f634176ed83bb7fd32702
Angel Gladin <angelgladin@gmail.com>
2020-11-25 00:18:08 -0600

    Algunas definiciones que estaré utilizando

---
+++ definiciones/filter.hs
@@ -0,0 +1,3 @@
+filter::(a -> Bool) -> [a] -> [a]
+filter p [] = []
+filter p (x:xs) = if p x then x:filter p xs else filter p xs
---
+++ definiciones/foldr.hs
@@ -0,0 +1,3 @@
+foldr :: (a -> b -> b) -> b -> [a] -> b
+foldr f e [] = e
+foldr f e (x:xs) = f x (foldr f e xs)
