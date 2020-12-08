e1c521f3a7d207e834082f1153e17571fb7d177d
Angel Gladin <angelgladin@gmail.com>
2020-12-07 19:34:37 -0600

    Por ahora es la versión final de la implementación

---
+++ codigo/haskell/mp-1.hs
@@ -0,0 +1,22 @@
+data Rep a
+  = Null
+  | Node a (Rep a) (Rep a)
+  deriving (Show)
+
+matches :: (Eq a) => [a] -> [a] -> [Int]
+matches ws = map fst . filter (ok . snd) . scanl step (0, root)
+  where
+    ok (Node (us, vs) l r) = null vs
+
+    step (n, t) x = (n + 1, op t x)
+    root = grep Null ([], ws)
+
+    op Null x = root
+    op (Node (us, []) l r) x = op l x
+    op (Node (us, v:vs) l r) x =
+      if v == x
+        then r
+        else op l x
+    
+    grep l (us, [])   = Node (us, []) l Null
+    grep l (us, v:vs) = Node (us, v : vs) l (grep (op l v) (us ++ [v], vs))
---
+++ codigo/haskell/mp-2.hs
@@ -0,0 +1,21 @@
+data Rep a
+  = Null
+  | Node a (Rep a) (Rep a)
+  deriving (Show)
+
+matches :: (Eq a) => [a] -> [a] -> [Int]
+matches ws = map fst . filter (ok . snd) . scanl step (0, root)
+  where
+    ok (Node vs l r) = null vs
+    step (n, t) x = (n + 1, op t x)
+
+    op Null x = root
+    op (Node [] l r) x = op l x
+    op (Node (v:vs) l r) x =
+      if v == x
+        then r
+        else op l x
+    
+    root = grep Null ws
+    grep l []     = Node [] l Null
+    grep l (v:vs) = Node (v : vs) l (grep (op l v) vs)
